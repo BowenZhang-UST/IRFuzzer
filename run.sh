200a235cd9df9db849583dfc6a26a655c8f6ccc2
BowenZhang-UST <bowen.zhang@connect.ust.hk>
2025-09-07 12:51:19 +0800

    x

--- run.sh
+++ run.sh
@@ -17,4 +17,4 @@ export ATTR=
 export MATCHER_TABLE_SIZE=13780
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 
-./AFLplusplus/afl-fuzz -i $FUZZING_HOME/seeds.ll -o fuzzing llvm-isel-afl/build/isel-fuzzing -V 10
+./AFLplusplus/afl-fuzz -i $FUZZING_HOME/seeds.ll -o fuzzing llvm-isel-afl/build/isel-fuzzing -V 50
